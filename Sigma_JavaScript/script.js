58ad3c497644cb1be3d4627e4f4121d380a57beb
Aakash <aakashjha343@gmail.com>
2024-06-15 14:05:08 +0530

    sigma 81 hacker's terminal exercise

---
+++ Sigma_JavaScript/script.js
@@ -0,0 +1,27 @@
+// alert("i am alert ");
+
+console.log("yo")
+console.log("ok")
+
+// var a = prompt("Enter the number : ")
+// console.log("this is your number " +  a )
+
+// var istrue = confirm("Are you sure to leave this site ? ")
+
+// if istrue {
+// if (istrue) {
+    // console.log("You are leaving the site")
+// } else {
+    // console.log("Stay on the site")
+// }
+
+
+
+
+
+
+
+
+
+
+
